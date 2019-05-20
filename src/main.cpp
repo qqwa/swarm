f9cea269e6ff14c7b18fa0b72a41ded4ddf0da1e
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2019-05-21 00:49:50 +0200

    basic main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main() {
+    GLFWwindow* window;
+
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    window = glfwCreateWindow(800, 600, "DebugWindow", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create window" << std::endl;
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+
+    // TODO: init opengl
+    glewExperimental = true;
+    if (glewInit() != GLEW_OK) {
+        std::cout << "Failed to initialze GLEW" << std::endl;
+        exit(1);
+    }
+
+    // TODO: init opencl
+
+    // TODO: initialize data, swarm members, mesh, etc...
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        // TODO: check if we need to calculate next frame
+
+        // TODO: draw frame
+        glClearColor(0.5, 0.5, 0.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+    }
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
